cecfb0ac8ba5e89c09558b9b4397308b06df502b
Marcelo Gomes <mars.gomes2003@gmail.com>
2024-04-10 21:49:34 +0100

    2.1

--- src/mrs.py
+++ src/mrs.py
@@ -11,12 +11,39 @@ import sounddevice as sd  #https://anaconda.org/conda-forge/python-sounddevice
 import warnings
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+audio_folder = "../MER_audio_taffc_dataset/songs"
+
+# Função para extrair todas as features necessárias de um arquivo de áudio
+def extract_features():
+    for file_name in os.listdir(audio_folder):
+        if os.path.isfile(os.path.join(audio_folder, file_name)):
+            file_path = os.path.join(audio_folder, file_name)
+            
+            features = []
+            
+            filey = librosa.load(file_path)
+            features.append(librosa.feature.mfcc(filey))
+            features.append(librosa.feature.spectral_centroid(filey))
+            features.append(librosa.feature.spectral_bandwidth(filey))
+            features.append(librosa.feature.spectral_contrast(filey))
+            features.append(librosa.feature.spectral_flatness(filey))
+            features.append(librosa.feature.spectral_rolloff(filey))
+            features.append(librosa.feature.yin(filey))
+            features.append(librosa.feature.rms(filey))
+            features.append(librosa.feature.zero_crossing_rate(filey))
+            features.append(librosa.beat.tempo(filey))
+            
+            print(features)
+
 
 if __name__ == "__main__":
     plt.close('all')
+    extract_features()
     
     #--- Load file
-    fName = "../Queries/MT0000414517.mp3"    
+    '''fName = "Queries/MT0000414517.mp3"    
     sr = 22050
     mono = True
     warnings.filterwarnings("ignore")
@@ -25,7 +52,7 @@ if __name__ == "__main__":
     print(fs)
     
     #--- Play Sound
-    sd.play(y, sr, blocking=False)
+    #sd.play(y, sr, blocking=False)
     
     #--- Plot sound waveform
     plt.figure()
@@ -37,14 +64,5 @@ if __name__ == "__main__":
     fig, ax = plt.subplots()
     img = librosa.display.specshow(Ydb, y_axis='linear', x_axis='time', ax=ax)
     ax.set_title('Power spectrogram')
-    fig.colorbar(img, ax=ax, format="%+2.0f dB")
-        
-    #--- Extract features    
-    sc = librosa.feature.spectral_centroid(y = y)  #default parameters: sr = 22050 Hz, mono, window length = frame length = 92.88 ms e hop length = 23.22 ms 
-    sc = sc[0, :]
-    print(sc.shape)
-    times = librosa.times_like(sc)
-    plt.figure(), plt.plot(times, sc)
-    plt.xlabel('Time (s)')
-    plt.title('Spectral Centroid')
+    fig.colorbar(img, ax=ax, format="%+2.0f dB")'''
     
